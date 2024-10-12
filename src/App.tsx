9629e9de3ba69a6c7452d71bf4a5677da7567139
kiineld <yaromyr.akimyv@gmail.com>
2024-10-12 23:09:19 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import {Button} from "@/components/ui/button.tsx";
+
+function App() {
+
+  return (
+      <>
+          <h1>Welcome to React!</h1>
+          <p>This is a simple React app.</p>
+          <Button className="btn">Click me</Button>
+      </>
+  )
+}
+
+export default App
