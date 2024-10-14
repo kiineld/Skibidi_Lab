d3899f4c705a74ba532e57d515e35fcdd0bf0855
kiineld <yaromyr.akimyv@gmail.com>
2024-10-15 00:23:22 +0300

    added_auth_verstka

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,1 +1,4 @@
 export { Button, buttonVariants } from "./button.tsx"
+export { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "./form.tsx"
+export { Label } from "./label.tsx"
+export { Input } from "./input.tsx"
---
+++ src/pages/about_project/aboutProject.tsx
@@ -0,0 +1,10 @@
+function AboutProject() {
+    return (
+        <div className="flex flex-col min-h-[95vh] min-w-full justify-center items-center">
+            <h1>Информация о нас и нашем проекте</h1>
+        </div>
+    )
+}
+
+export default AboutProject
+
