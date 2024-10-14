ca0fa77a3e74554d788f0e9c978ad4de894fd0e0
kiineld <yaromyr.akimyv@gmail.com>
2024-10-15 00:35:42 +0300

    added_auth_verstka_vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
 import path from "path"
 import react from "@vitejs/plugin-react"
+import vercel from 'vite-plugin-vercel';
 import { defineConfig } from "vite"
 
 export default defineConfig({
-  plugins: [react()],
+  server: {
+    port: process.env.PORT as unknown as number
+  },
+  define: {
+    __APP_ENV__: process.env.VITE_VERCEL_ENV,
+  },
+  plugins: [react(), vercel()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
